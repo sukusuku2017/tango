d912b796f02337b03c36829827a69705e106f0b0
GimYoungPhil <youngphil.gim@gmail.com>
2017-02-17 23:42:42 +0900

    add st14, 15

--- csv/sentence.js
+++ csv/sentence.js
@@ -3,7 +3,7 @@ const csv = require('csvtojson')
 const _ = require('underscore');
 const jsonfile = require('jsonfile');
 
-const CHAPTER = '13';
+const CHAPTER = '15';
 let tempArray = [];
 let result = {
   "chapter" : CHAPTER,
